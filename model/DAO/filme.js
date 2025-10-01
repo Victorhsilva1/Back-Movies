2a60ddf742ac9130787bc67c1595f5fff0d9711d
Victorhsilva1 <victor.hsilva2703@gmail.com>
2025-10-01 09:04:21 -0300

    dao atualizado com as funçoes

--- model/DAO/filme.js
+++ model/DAO/filme.js
@@ -5,3 +5,30 @@
  * Versão: 1.0
  **************************/
 
+//Função que retorna todos os filmes do banco de dados
+// async por conta da assincrona, para que espere com Await
+const getSelectAllMovies = async function() {
+
+}
+
+//Função que retorna pelo ID o filme do banco de dados
+//Com argumento ID
+const getSelectByIdMovies = async function(id) {
+
+}
+
+//Função que insere um filme novo no banco de dados
+const setInsertMovies = async function() {
+
+}
+
+//Função que altera um filme no banco de dados
+const setUpdateMovies = async function(id) {
+
+}
+
+//Função que deleta um filme pelo ID no banco de dados
+const setDeleteMovies = async function(id) {
+
+
+}
